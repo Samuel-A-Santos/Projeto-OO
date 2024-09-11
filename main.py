7af7619ba64ac4a93aac3d70e04dcf3e6ef70395
Samuel Alessandro Lima dos Santos <samuel@DESKTOP-7FDCQPP>
2024-09-11 16:53:50 -0300

    wip

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import services.database as db
 from app import create_app
 
-db.conectar_banco()
+db.conectar_banco()  # Certifique-se de que a conexão com o banco de dados é inicializada
 
 if __name__ == "__main__":
     app = create_app()
